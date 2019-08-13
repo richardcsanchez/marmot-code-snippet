267c72f7f688fc9dfd0300ca96694a077265ce2d
Richard Sanchez <richard.charles.s@gmail.com>
2019-08-13 17:09:14 -0400

    move window scroll trigger and connect to openPopUp

--- console-code.js
+++ console-code.js
@@ -1,16 +1,4 @@
-$(window).on("scroll", function() {
 
-		var body = document.body;
-		var html = document.documentElement;
-
-		var height = Math.max( body.scrollHeight, body.offsetHeight,
-		                       html.clientHeight, html.scrollHeight, html.offsetHeight );
-
-		var scrollAmount = window.scrollY;
-		    if (scrollAmount > ((height-window.innerHeight)*.9)) {
-		    alert("test");
-		    }
-});
 
 function getItems () {
 	var products = [];
@@ -88,3 +76,17 @@ function openPopUp() {
   }
 
 }
+
+$(window).on("scroll", function() {
+
+		var body = document.body;
+		var html = document.documentElement;
+
+		var height = Math.max( body.scrollHeight, body.offsetHeight,
+		                       html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+		var scrollAmount = window.scrollY;
+		    if (scrollAmount > ((height-window.innerHeight)*.9)) {
+		    openPopUp();
+		    }
+});
