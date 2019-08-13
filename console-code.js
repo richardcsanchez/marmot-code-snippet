75c89c0578d1d07226bfb54b81128e1378a3bc27
Richard Sanchez <richard.charles.s@gmail.com>
2019-08-13 16:24:54 -0400

    save image and name into products array

--- console-code.js
+++ console-code.js
@@ -11,3 +11,22 @@ $(window).on("scroll", function() {
 		    alert("test");
 		    }
 });
+
+function getItems () {
+	var products = [];
+
+	const cartProducts = document.querySelectorAll(".mini-cart-product");
+
+  for (var i = 0; i < cartProducts.length; i++) {
+    const product = cartProducts[i];
+    const image = product.querySelector('.mini-cart-image img').src
+    const name = product.querySelector(".mini-cart-name").innerText
+
+    products.push({
+      name,
+      image
+    })
+  }
+  
+	return products;
+}
